c5cfdcc9285a23d66e28320e196ee3c49e236610
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2021-11-12 18:44:46 -0800

    ...

--- 2020/day_02/day_02.py
+++ 2020/day_02/day_02.py
@@ -12,4 +12,17 @@ for line in open('input.txt'):
 
 print(f'{len(goodies)} valid passwords, damn! those are some satanic toboggans, I like it')
 
+# part two
+goodies = list()
+for line in open('input.txt'):
+    line = line.strip()
+    rule, password = line.split(':')
+    password = password.strip()
+    idxs, letter = rule.split(' ')
+    vil = [password[int(i)-1] for i in idxs.split('-')] # very important letters
 
+    ocnum = vil.count(letter)
+    if ocnum == 1:
+        goodies.append(line)
+
+print(f'{len(goodies)} valid passwords, less cool')
