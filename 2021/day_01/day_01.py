215d6d47b50ebc8726f4462158455d4b1f8d91ef
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2021-12-01 15:34:49 -0800

    add vectorized solution to 2021 day 1, 20+ times faster

--- 2021/day_01/day_01.py
+++ 2021/day_01/day_01.py
@@ -1,4 +1,5 @@
 import numpy as np
+import timeit
 with open('input.txt') as file:
     depths = np.asarray([int(line) for line in file])
 
@@ -7,5 +8,28 @@ print(depths.shape, depths, )
 print(f"measurmemetns greater than previous: {np.sum(np.diff(depths) > 0)}")
 
 # part 2
-windowed = np.stack([depths[i:i+3] for i in range(depths.size-2)], axis=0)
-print(f"windowed greater than previous: {np.sum(np.diff(np.sum(windowed,axis=1)) > 0)}")
+def non_vect():
+    window_size = 3
+    max_time = depths.size - window_size + 1
+    windowed = np.stack([depths[i:i+window_size] for i in range(max_time)], axis=0)
+    sum_increase = np.sum(np.diff(np.sum(windowed,axis=1)) > 0)
+    return sum_increase
+
+# alternative
+def vect():
+    window_size = 3
+    max_time = depths.size - window_size + 1
+    win_idx = (np.expand_dims(np.arange(window_size), axis=0) +
+               np.expand_dims(np.arange(max_time), axis=1) )
+    windowed = depths[win_idx]
+    sum_increase = np.sum(np.diff(np.sum(windowed, axis=1)) > 0)
+    return sum_increase
+
+print(f"windowed greater than previous: {vect()}")
+
+
+
+nv_time = timeit.timeit(non_vect, number=1000)
+v_time = timeit.timeit(vect, number=1000)
+print(f'non_vectorizes:{nv_time:.2f}, vectorized:{v_time:.2f}, improvement:{nv_time/v_time:.2f} times faster')
+
