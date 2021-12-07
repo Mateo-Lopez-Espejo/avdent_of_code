872a9f05d9a3952291ed3a45848aff4082e7132f
mateo <mateo@pop-os.localdomain>
2021-12-06 23:03:26 -0800

    some recursion on day 6, it is driving me insane!

--- 2021/day_06/day_06.py
+++ 2021/day_06/day_06.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 start_pop_vctr = np.loadtxt('input.txt', dtype=int, delimiter=',')
 
 
@@ -27,6 +28,65 @@ sim_vctr = sim_generatiosn(start_pop_vctr, days)
 print(f'there is {sim_vctr.shape[0]} fish after {days}')
 
 # part 2
+# define a function to calculate direct offspring given n days
+def direct_offspring(pop, stage, time):
+    cycle_raw = (time-stage+5)/6
+    cycle_poss = np.maximum(np.zeros_like(cycle_raw), cycle_raw) # negatives to zero
+    cycle_int = np.floor(cycle_poss) # integers for discrete time points
+    offspring = pop * cycle_int # took me a while, empirically tested
+    return offspring
+
+
+# # empirical test, dont ask me, Im a shitty data scientist
+# fig , ax = plt.subplots()
+# for ss in range(9):
+#     ofp = list()
+#     for tt in range(30):
+#         ofp.append(direct_offspring(1, ss, tt))
+#
+#     ax.plot(ofp,label=f'{ss}')
+# ax.legend()
+# fig.show()
+
+
+# reccursively call offspring function for children
+
+
+def recurse_linearity(pop, stage, time, cum_pop=0):
+    for tt in range(time):
+        do = direct_offspring(pop, stage, tt)
+        if any(do > 0):
+            cum_pop += recurse_linearity(do[do>0],8,time-tt, cum_pop=cum_pop)
+    else:
+        return np.sum(pop) + cum_pop
+
+
+yyy = list()
+for t in range(5):
+    yyy.append(recurse_linearity(np.asarray([1]),
+                      np.asarray([0]),
+                      t))
+
+
+fig, ax = plt.subplots()
+ax.plot(yyy)
+fig.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 days = 256
 sim_vctr = sim_generatiosn(start_pop_vctr, days)
 print(f'there is {sim_vctr.shape[0]} fish after {days}')
