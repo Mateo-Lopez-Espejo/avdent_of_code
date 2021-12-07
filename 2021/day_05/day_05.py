1ea850ccc8c3e089fc7869680a7e4f1b2b3e3419
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2021-12-06 16:11:11 -0800

    finished day 5

--- 2021/day_05/day_05.py
+++ 2021/day_05/day_05.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open('input.txt') as file:
     coor_vctrs = list()
@@ -10,10 +11,37 @@ with open('input.txt') as file:
         start.extend(end)
         coor_vctrs.append(start)
 
+
+# array with shape Lines x Coordinates, where Coordinates dimension has positions: x0,y0,x1,y1
 coor_vctrs = np.asarray(coor_vctrs)
+
+# transforms coordinates into grid positions
+# init grid stack for speed
+grid_stack = np.zeros((coor_vctrs.shape[0],               # coords
+                       np.max(coor_vctrs[:,[0,2]])+1,     # x
+                       np.max(coor_vctrs[:,[1,3]])+1))    # y
+
+for vv, vctr in enumerate(coor_vctrs):
+    # here is the magic, transforms the coordinates into vector indices
+    x = np.linspace(vctr[0], vctr[2], np.abs(vctr[0]-vctr[2])+1, endpoint=True).astype(int)
+    y = np.linspace(vctr[1], vctr[3], np.abs(vctr[1]-vctr[3])+1, endpoint=True).astype(int)
+    # using vctr indices follow broadcast rules for numpy so if one vctr is a singleton broadcasts
+    grid_stack[vv,x,y] = 1
 
 
 # part 1
+vert = coor_vctrs[:, 0] == coor_vctrs[:, 2]
+hori = coor_vctrs[:, 1] == coor_vctrs[:, 3]
 
-horizontal = coor_vctrs
-subset = coor_vctrs
+subset = grid_stack[vert | hori, ...]
+
+overlaps = np.sum(subset, axis=0)
+danger_zone_count = np.sum(overlaps>=2)
+print(f'there is {danger_zone_count} DANGER ZONES!')
+
+
+# part 2
+overlaps = np.sum(grid_stack, axis=0)
+danger_zone_count = np.sum(overlaps>=2)
+print(f'even more!:{danger_zone_count} DANGER ZONES!')
+
