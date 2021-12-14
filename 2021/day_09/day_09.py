38cfa7948039ebbebdcc91658d89b02851dc82c1
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2021-12-13 19:23:20 -0800

    finished day 9... after 4 days, and following rich strategy. Kill me now

--- 2021/day_09/day_09.py
+++ 2021/day_09/day_09.py
@@ -1,98 +1,56 @@
 import numpy as np
-import seaborn as sns
 
-
-# with open('input.txt') as file:
-with open('test.txt') as file:
+with open('input.txt') as file:
+# with open('test.txt') as file:
     floormap = list()
     for line in file:
         floormap.append([int(i) for i in line.strip()])
     floormap = np.asarray(floormap)
 
 
-padded = np.full(np.asarray(floormap.shape)+2, 10)
-padded[1:-1, 1:-1] = floormap
-islow = np.zeros_like(floormap).astype(bool)
-
-
-# part 1
-lows_sum = 0
-for index in np.ndindex(floormap.shape):
-    pad_index = np.asarray(index) + 1
-    neighbour = {'up'    : padded[pad_index[0]-1, pad_index[1]],
-                 'down'  : padded[pad_index[0]+1, pad_index[1]],
-                 'left'  : padded[pad_index[0], pad_index[1]-1],
-                 'right' : padded[pad_index[0], pad_index[1]+1]}
-
-    neigh_vctr = np.asarray([v for v in neighbour.values()])
-
-    if np.all(neigh_vctr > floormap[index]):
-        lows_sum += floormap[index] + 1
-        islow[index] = True
-
-# sns.heatmap(islow,)
-
-print(lows_sum)
-
-
-# part 2
 padded = np.full(np.asarray(floormap.shape)+2, 9)
 padded[1:-1, 1:-1] = floormap
-basin_map = np.full_like(padded, 0)
-basin_map[padded == 9] = -1
 
-basin_counter = 1
+# part 1
+# movement relative to padded array
+up, down, left, right = np.s_[1:-1,:-2], np.s_[1:-1,2:], np.s_[0:-2,1:-1], np.s_[2:,1:-1]
+offsets = [up,down,left,right]
 
-# has two make two passes, forwards and bakwards
-# indices = list(np.ndindex(floormap.shape))
-# indices = indices + indices[::-1]
+islow = np.full((4,)+floormap.shape, False)
+for oo, offset in enumerate(offsets):
+    islow[oo,...] = floormap < padded[offset]
 
-for index in np.ndindex(floormap.shape):
-    pad_index = np.asarray(index) + 1
+islow = np.all(islow, axis=0)
+print(f'sum risk of low points: {np.sum(floormap[islow]+1)}')
 
-    neig_idx = {'it'    : pad_index,
-                'up'    : pad_index - [1, 0],
-                'down'  : pad_index + [1, 0],
-                'left'  : pad_index - [0, 1],
-                'right' : pad_index + [0, 1]}
+# part 2
+# makes a map of basin sources and limits
 
-    arr_idx = np.asarray(list(neig_idx.values()))
-    neig_idx = tuple(ii for ii in arr_idx.T)
-
-    neigh_vctr = basin_map[neig_idx]
+edges = floormap == 9
 
 
-    if neigh_vctr[0] != -1:
-        valid_idx = list()
-        for vp in arr_idx:
-            if basin_map[vp[0], vp[1]] != -1:
-                valid_idx.append(vp)
+basin_map = np.full_like(floormap, 0)
+basin_map[floormap == 9] = -1
+for nn, (xx, yy) in enumerate(np.argwhere(islow)):
+    basin_map[xx, yy] = nn + 1
 
-        valid_idx = tuple(ii for ii in np.asarray(valid_idx).T)
+basin_pad = np.full(np.asarray(basin_map.shape)+2, -1)
+basin_pad[1:-1, 1:-1] = basin_map
 
-        if min(basin_map[valid_idx]) == 0:
-            basin_map[valid_idx] = basin_counter
-            basin_counter += 1
-        else:
-            basin_map[valid_idx] = min(basin_map[valid_idx])
+# inpired by rich.
+changing = True
+while changing:
+    for offset in offsets:
+        basin_map = np.maximum(basin_map, basin_pad[offset])
+        basin_map[edges] = -1
 
-sns.heatmap(basin_map.astype(int), annot=True)
+    if np.all(basin_map == basin_pad[1:-1, 1:-1]):
+        changing = False
 
-
-
-
-
-
+    basin_pad[1:-1, 1:-1] = basin_map
 
 
+basin_idx, basin_size = np.unique(basin_map[~edges], return_counts=True)
+basin_size.sort()
 
-
-
-
-
-
-
-
-
-
-
+print(np.prod(basin_size[-3:]))
